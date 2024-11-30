923df78c6bee749f4b43cb807c865f63711689a1
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-12-01 00:54:17 +0900

    入力フォームの送信トグルボタンを作成

--- lib/widgets/toggle_button.dart
+++ lib/widgets/toggle_button.dart
@@ -26,9 +26,57 @@ class ToggleButton extends StatefulWidget {
   State<ToggleButton> createState() => _ToggleButtonState();
 }
 
-class _ToggleButtonState extends State<ToggleButton> {
+class _ToggleButtonState extends State<ToggleButton>
+ with SingleTickerProviderStateMixin{
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final iconColor = Theme.of(context).colorScheme.onSecondary;
+    final padding = MediaQuery.of(context).size.width * 0.025;
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+            foregroundColor: widget._isReplying ? Colors.grey : iconColor,
+            shape: const CircleBorder(),
+            padding: EdgeInsets.all(padding),
+          ),
+          // 返信中はボタン無効化
+          onPressed: widget._isReplying
+            ? null
+            : (widget._inputMode == InputMode.text
+                ? widget._sendTextMessage
+                : () {
+                    widget._sendVoiceMessage();
+                  }),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                if (widget._isListening)
+                  BoxShadow(
+                    color: Colors.redAccent.withOpacity(0.4),
+                    spreadRadius: 2,
+                    blurRadius: 4,
+                    offset: const Offset(0, 1),
+                  ),
+              ],
+            ),
+            child: Icon(
+              // テキストモードか音声モードかでアイコンを切り替える
+              widget._inputMode == InputMode.text
+                  ? Icons.send
+                  : widget._isListening
+                    ? Icons.mic_off
+                    : Icons.mic,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
